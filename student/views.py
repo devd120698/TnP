080cd35d96090ce2898aa8b3994337c2c199df91
ykomal12345 <ykomal678@gmail.com>
2020-07-06 19:54:05 +0530

    home page modular

--- student/views.py
+++ student/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 from coordinator.models import *
 from datetime import datetime, timedelta
+from django.core.mail import send_mail
 
 listOfAnnouncements = []
 noOfAnnouncements = 0
@@ -17,14 +18,13 @@ student  = None
 
 @login_required
 def studentDashboard(request):
-    
     global listOfAnnouncements, student, noOfAnnouncements
     return render(request, 'student/dashboard/index.html', {'student':student,'noOfAnnouncements': noOfAnnouncements })
 
 @login_required
 def registerStudent(request):
     user = request.user
-    global student
+    student = Student.objects.get(user = request.user)
     if Student.objects.filter(user = user).exists() :
         getAnnouncements = Announcement.objects.filter(datePublished__gte = datetime.now() - timedelta(1), datePublished__lte = datetime.now())
         student = Student.objects.get(user = request.user)
@@ -52,7 +52,7 @@ def registerStudent(request):
 
 @login_required
 def viewNewApplications(request):
-    global student
+    student = Student.objects.get(user = request.user)
 
     user = request.user
     company = CompanyApplicants.objects.filter(student = student).filter(placementStatus = 'N')
@@ -64,19 +64,28 @@ def viewNewApplications(request):
         applicantData = CompanyApplicants.objects.get(student = student, company = companyDetails)
         applicantData.placementStatus = 'A'
         applicantData.save()
+
+        text_to_be_sent = 'Dear ' + student.name + ',\n' + 'You have successfully applied to be a part of the placement drive for the company - ' +  companyName + '. We will be reaching out to you with further notifications about the process.\n' + 'Best Regards\n' + 'CCPD.'
+        send_mail(
+		    'Application Confirmation',
+		    text_to_be_sent,
+		    'taps@nitw.ac.in',
+		    [request.user.email],
+		    fail_silently=True,
+	    )
         
     return render(request,'student/showCompanies.html',context)
 
 @login_required
 def viewStatusOfApplication(request):
-    global student
+    student = Student.objects.get(user = request.user)
     user = request.user
     company = CompanyApplicants.objects.filter(student = student).exclude(placementStatus = 'N').exclude(placementStatus = 'R')
     return render(request,'student/showApplied.html',{'eligibleCompanies':company, 'student':student})
 
 @login_required
 def viewProfile(request):
-    global student
+    student = Student.objects.get(user = request.user)
 
     resumeUploaded = False
     if Resume.objects.filter(user = request.user).exists():
@@ -85,7 +94,7 @@ def viewProfile(request):
 
 @login_required
 def uploadResume(request):
-    global student
+    student = Student.objects.get(user = request.user)
     
     form = ResumeForm(request.POST or None)
     if form.is_valid():
@@ -110,7 +119,7 @@ def uploadResume(request):
 
 @login_required
 def showCalendar(request):
-    global student
+    student = Student.objects.get(user = request.user)
     return render(request,'student/dashboard/pages/calendar.html',{'student':student})
 
 @login_required
