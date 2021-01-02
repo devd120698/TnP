26cefea61b3e4c037003bff2e89ad9a804da7f13
spor3006 <41056517+spor3006@users.noreply.github.com>
2021-01-02 23:28:21 +0530

    removed_gitignore

---
+++ tnp/databases.py
@@ -0,0 +1,28 @@
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default': {
+        'NAME': 'tnp',
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '',
+    },
+    'wsdc_student': {
+        'NAME': 'wsdc_student',
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '',
+    },
+
+    'coordinators': {
+        'NAME': 'coordinators',
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': '',
+    }
+}
